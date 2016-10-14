f2c0d255589124703d77ad3857e08605edc6475e
chriskohlbrenner <c.kohlbrenner@gmail.com>
2016-10-14 19:59:50 -0400

    api responses

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 require('express-namespace');
+const bodyParser = require('body-parser')
 const app = express();
+app.use(bodyParser.json());
 
 const db_config = require('./knexfile');
 const knex = require('knex')(db_config);
@@ -14,14 +16,44 @@ app.listen(3000, function() {
 
   app.namespace('/api', function() {
     app.namespace('/games', function() {
+      // CREATE new game
       app.post('/new', function(req, res) {
-        res.send("CREATE NEW GAME")
+        knex('games').insert({completed: false}, 'id')
+        .then(function(id) {
+          res.send({id: id[0]});
+        });
       });
+      // DELETE game
       app.delete('/:id', function(req, res) {
-        res.send("DELETE GAME")
+        const id = req.params.id
+        knex('games').where('id', id).delete()
+        .then(function() {
+          res.send({id});
+        });
       });
-      app.put('/:id', function(req, res) {
-        res.send("UPDATE GAME")
+      // POST placements
+      app.post('/:id/placement', function(req, res) {
+        const {x_pos, y_pos, player_number} = req.body
+        const game_id = req.params.id
+        knex('placements').insert({
+          game_id: game_id,
+          x_pos, y_pos, player_number
+        }, 'id')
+        .then(function(id) {
+          res.send({game_id, placement_id: id[0]});
+        })
+      });
+      // POST moves
+      app.post('/:id/moves', function(req, res) {
+        const {x_pos, y_pos, player_number} = req.body;
+        const game_id = req.params.id;
+        knex('movess').insert({
+          game_id: game_id,
+          x_pos, y_pos, player_number
+        }, 'id')
+        .then(function(id) {
+          res.send({game_id, placement_id: id[0]});
+        })
       });
     });
   });
